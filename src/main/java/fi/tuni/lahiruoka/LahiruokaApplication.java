af4ce6f1d69033436049f8c1a5f91cb96388bbb4
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-04 11:30:15 +0200

    Initial commit.

---
+++ src/main/java/fi/tuni/lahiruoka/LahiruokaApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.lahiruoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LahiruokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LahiruokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/tuni/lahiruoka/LahiruokaApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.tuni.lahiruoka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LahiruokaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
