52d18a1169c21c3bac66efd784b9b07c7de3c3bf
Anu Malm <anu.m.malm@gmail.com>
2019-03-11 14:15:33 +0200

    Product.java: Add constructors + getters and setters for parameters.

--- src/main/java/fi/tuni/lahiruoka/Product.java
+++ src/main/java/fi/tuni/lahiruoka/Product.java
@@ -22,4 +22,70 @@ public class Product {
     @Column(nullable = false)
     LocalDate availableTo;
 
+    public Product() {}
+
+    public Product(String name, double price, int units, double unitSize, LocalDate availableFrom, LocalDate availableTo) {
+        this.name = name;
+        this.price = price;
+        this.units = units;
+        this.unitSize = unitSize;
+        this.availableFrom = availableFrom;
+        this.availableTo = availableTo;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+
+    public double getUnitSize() {
+        return unitSize;
+    }
+
+    public void setUnitSize(double unitSize) {
+        this.unitSize = unitSize;
+    }
+
+    public LocalDate getAvailableFrom() {
+        return availableFrom;
+    }
+
+    public void setAvailableFrom(LocalDate availableFrom) {
+        this.availableFrom = availableFrom;
+    }
+
+    public LocalDate getAvailableTo() {
+        return availableTo;
+    }
+
+    public void setAvailableTo(LocalDate availableTo) {
+        this.availableTo = availableTo;
+    }
 }
