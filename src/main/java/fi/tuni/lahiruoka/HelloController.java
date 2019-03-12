a301704ad8d2694bb1cab191006db52aa3322e7c
Anu Malm <anu.m.malm@gmail.com>
2019-03-12 11:16:24 +0200

    HelloController.java: Test that both user and product tables work.

--- src/main/java/fi/tuni/lahiruoka/HelloController.java
+++ src/main/java/fi/tuni/lahiruoka/HelloController.java
@@ -15,17 +15,22 @@ public class HelloController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     // To see table description, use SHOW COLUMNS from PRODUCT; in h2.
     @PostConstruct
     public void init() {
         //String name, double price, int units, double unitSize, LocalDate availableFrom, LocalDate availableTo
         productRepository.save(new Product("Porkkana", 2.3, 10, 1.0, LocalDate.of(2019, 3, 12), LocalDate.of(2019, 4, 6)));
         productRepository.save(new Product("Peruna", 1.3, 15, 2.0, LocalDate.of(2019, 2, 10), LocalDate.of(2019, 12, 24)));
-
+        //String username, String password, LocalDate lastLogin, String address, String phone, boolean active
+        userRepository.save(new User("henkilo", "salasana", LocalDate.of(2019,03,12), "joku osoite 450", "049857", true));
+        userRepository.save(new User("ukkeli", "salasana", LocalDate.of(2019,03,13), "toinen osote 444", "546224", true));
     }
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        return "User repo on " + userRepository.findAll() + "\n" + ", product repo on " + productRepository.findAll() + "\n";
     }
 }
