c753eb59356aff7f4ac10912ecd2e24575f524ec
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-12 13:18:53 +0200

    App.js: Show Header
    Use React-component to show new Header. Header includes navigation and a couple of links.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './css/App.css';
+import Header from './Header';
 
 class App extends Component {
 
   state = {};
 
-  componentDidMount() {
-      setInterval(this.hello, 250);
-  }
-
-  hello = () => {
-      fetch('/api/hello')
-          .then(response => response.text())
-          .then(message => {
-              this.setState({msg: message});
-              console.log(this.state.msg);
-          });
-  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h3>{this.state.msg}</h3>
-        </header>
-      </div>
+        <Header />
     );
   }
 }
