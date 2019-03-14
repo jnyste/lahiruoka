47bf99d2a90cf516ed27bd7e34e609044edb6832
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2019-03-14 23:17:11 +0200

    Tag.java: Add getters and setters to Tag class.

--- src/main/java/fi/tuni/lahiruoka/Tag.java
+++ src/main/java/fi/tuni/lahiruoka/Tag.java
@@ -37,4 +37,36 @@ public class Tag {
     }
 
     // Getters and Setters (Omitted for brevity)
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
