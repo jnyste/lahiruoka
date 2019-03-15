5195120591fe693e5cb4e146ad0581d6975069ae
Anu Malm <anu.m.malm@gmail.com>
2019-03-15 12:37:12 +0200

    Create ProductPage.js
    sIncluded already constructor(), componentDidMount() and render().

---
+++ frontend/src/ProductPage.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import './css/ProductPage_style.css';
+
+class ProductPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <p>Hahaa</p>
+        );
+    }
+}
