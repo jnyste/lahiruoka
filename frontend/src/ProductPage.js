2c0a25d3138a5c5d0aa51e1bd467604c64d3272f
Anu Malm <anu.m.malm@gmail.com>
2019-03-15 13:49:15 +0200

    ProductPage.js: Add basic code.
    Start of divs etc.

--- frontend/src/ProductPage.js
+++ frontend/src/ProductPage.js
@@ -13,7 +13,29 @@ class ProductPage extends Component {
 
     render() {
         return (
-            <p>Hahaa</p>
+            <div className="productbox">
+                <div className="productinfo">
+                    <h3 className="product title">Porkkana</h3>
+                    <p className="product owner">Esimerkin tila</p>
+                    <p>Saatavilla yht. 300kg</p>
+                    <p>Erätiedot:</p>
+                    <p className="tags">#tag #porkkana #epic</p>
+                </div>
+                <div className="order">
+                    <select>
+                        <option value="0">Valitse erä...</option>
+                        <option value="1">5kg</option>
+                        <option value="2">10kg</option>
+                    </select>
+                    <form>
+                        <input type="text" name="kpl" />
+                        <p>Kpl</p>
+                    </form>
+                    <input type="date" name="deliverydate" id="deliverypicker"/>
+                </div>
+            </div>
         );
     }
 }
+
+export default ProductPage;
