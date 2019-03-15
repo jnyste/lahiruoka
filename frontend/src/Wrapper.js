429f2769d6ce86ffae4795862b5367605b3c1e3d
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-03-15 11:26:03 +0200

    header_style.css renamed to frontpage_style.css

--- frontend/src/Wrapper.js
+++ frontend/src/Wrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './css/header_style.css';
+import './css/frontpage_style.css';
 import FarmCarousel from './FarmCarousel';
 
 function Index() {
