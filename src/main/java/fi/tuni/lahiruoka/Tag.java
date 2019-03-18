2ca1e8f4cd96941a29f83f31c65d7b22e3b669b2
Anu Malm <anu.m.malm@gmail.com>
2019-03-18 12:17:58 +0200

    Tag.java: Add JsonIgnore-annotation to prevent infinite loop.

--- src/main/java/fi/tuni/lahiruoka/Tag.java
+++ src/main/java/fi/tuni/lahiruoka/Tag.java
@@ -2,6 +2,7 @@ package fi.tuni.lahiruoka;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NaturalId;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -20,6 +21,7 @@ public class Tag {
     @NaturalId
     private String name;
 
+    @JsonIgnore
     @ManyToMany(fetch = FetchType.LAZY,
             cascade = {
                     CascadeType.PERSIST,
