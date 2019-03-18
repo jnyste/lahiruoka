cf10bd806aa20306b32642d1f8f286b3144a5cf5
Anu Malm <anu.m.malm@gmail.com>
2019-03-18 11:10:40 +0200

    Create UserProfile.js.
    Includes already componentDidMount()-methos and render-method.

---
+++ frontend/src/UserProfile.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import './css/FarmCarousel_style.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class UserProfile extends Component {
+
+    state = {};
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <h1>Profiili</h1>
+            )
+    }
+}
+
+export default UserProfile;
