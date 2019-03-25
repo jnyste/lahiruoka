ff0b4ceb5aedde171d254367128e1b886858921a
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-25 16:39:37 +0200

    Create AddProduct.js
    For adding a product to user profile.

---
+++ frontend/src/AddProduct.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class AddProduct extends Component {
+
+    render() {
+        return (
+            <p>uliuil</p>
+        );
+    }
+}
+
+export default AddProduct;
