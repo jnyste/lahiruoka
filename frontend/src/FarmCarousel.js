8e765a9e0ad39ce55aeaf1da4c610e8a9ae261a1
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-25 16:02:48 +0200

    FarmCarousel.js: Create CarouselItem-component that is rendered to carousel.
    First carousel item is kind of like Welcome-screen, other carousel items feature farm users.

--- frontend/src/FarmCarousel.js
+++ frontend/src/FarmCarousel.js
@@ -3,7 +3,22 @@ import './css/FarmCarousel_style.css';
 import logo1 from './carrot.jpeg';
 import logo2 from './tomato.jpeg';
 import logo3 from './wheat.jpeg';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class CarouselItem extends Component {
+    render() {
+        return (
+            <div className="item">
+                <a href="http://www.bing.com">
+                    <img src={logo3} alt="Vehnä"/>
+                </a>
+                <div className="carousel-caption">
+                    <h3>{this.props.item.companyName}</h3>
+                    <p>Täällä on tuotteita, esim.</p>
+                </div>
+            </div>
+        )
+    }
+}
 
 class FarmCarousel extends Component {
 
@@ -13,9 +28,16 @@ class FarmCarousel extends Component {
         fetch('/api/users')
             .then(response => response.json())
             .then(user => {
-                let farmOne = user[0];
-                let farmUsername = farmOne.companyName;
-                this.setState({farmOne: farmUsername});
+                let helperArray = [];
+
+                for (let i = 0; i < 2; i++) {
+                    let obj = user[i];
+                    console.log(obj);
+                    helperArray.push(<CarouselItem key={obj.id} item={obj}/>);
+                }
+
+                this.setState({carouselItemList: helperArray});
+
             });
     }
 
@@ -36,30 +58,12 @@ class FarmCarousel extends Component {
                                 <img src={logo1} alt="Porkkana" />
                             </a>
                             <div className="carousel-caption">
-                                <h3>{this.state.farmOne}</h3>
-                                <p>Täällä on tuotteita, esim.</p>
+                                <h3>Tervetuloa lähiruokasivulle</h3>
+                                <p>On aika siistii et löysit tänne. Tervetuloa!</p>
                             </div>
                         </div>
 
-                        <div className="item">
-                            <a href="http://www.bing.com">
-                                <img src={logo2} alt="Tomaatti"/>
-                            </a>
-                            <div className="carousel-caption">
-                                <h3>Mummolan tila</h3>
-                                <p>Täällä on tuotteita, esim.</p>
-                            </div>
-                        </div>
-
-                        <div className="item">
-                            <a href="http://www.duckduckgo.com">
-                                <img src={logo3} alt="Vehnä"/>
-                            </a>
-                            <div className="carousel-caption">
-                                <h3>Mielen tila</h3>
-                                <p>Täällä on tuotteita, esim.</p>
-                            </div>
-                        </div>
+                        {this.state.carouselItemList}
                     </div>
 
                     <a className="left carousel-control" href="#myCarousel" data-slide="prev">
