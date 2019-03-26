9a51d9dc0541afcb727d5d53de5e44af4e9edc61
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-03-26 13:19:52 +0200

    AddProduct.js: Add form start.

--- frontend/src/AddProduct.js
+++ frontend/src/AddProduct.js
@@ -1,11 +1,47 @@
 import React, {Component} from "react";
+import './css/AddProduct_style.css';
 
 class AddProduct extends Component {
 
     render() {
         return (
-            <p>uliuil</p>
-        );
+            <div className="product-add-form">
+                <h2>Lisää tuote</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="exampleProductName">Tuotteen nimi</label>
+                        <input type="text" className="form-control" id="exampleProductName"
+                               placeholder="Anna tuotteen nimi"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="examplePrice">Veroton hinta per kg</label>
+                        <input type="text" className="form-control" id="examplePrice" placeholder="Veroton hinta (kg)"/>
+                        <small id="priceHelp" className="form-text text-muted">Hintaan lisätään 14% ALV oston yhteydessä.
+                        </small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleAmount">Määrä kiloina</label>
+                        <input type="text" className="form-control" id="exampleAmount" placeholder="Määrä(kg)"/>
+                    </div>
+                    <label htmlFor="exampleAmount">Saatavilla:</label>
+                    <div className="row">
+                        <div className="col">
+                            <input type="date" className="form-control" />
+                        </div>
+                        <p>-</p>
+                        <div className="col">
+                            <input type="date" className="form-control" />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="exampleInfo">Lisätiedot</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Lisää</button>
+                </form>
+            </div>
+        )
     }
 }
 
