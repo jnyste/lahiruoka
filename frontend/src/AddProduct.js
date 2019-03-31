f00454e64941b5c46248b8d191634e2f9d6278bc
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-01 01:01:59 +0300

    AddProduct.js: Add "required"-classname to divs with mandatory info so that the fields are highlighted.

--- frontend/src/AddProduct.js
+++ frontend/src/AddProduct.js
@@ -7,23 +7,26 @@ class AddProduct extends Component {
         return (
             <div className="product-add-form">
                 <h2>Lisää tuote</h2>
+                <p>Tähdellä merkityt kohdat ovat pakollisia.</p>
                 <form>
-                    <div className="form-group">
+                    <div className="form-group required">
                         <label htmlFor="exampleProductName">Tuotteen nimi</label>
                         <input type="text" className="form-control" id="exampleProductName"
                                placeholder="Anna tuotteen nimi"/>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group  required">
                         <label htmlFor="examplePrice">Veroton hinta per kg</label>
                         <input type="text" className="form-control" id="examplePrice" placeholder="Veroton hinta (kg)"/>
                         <small id="priceHelp" className="form-text text-muted">Hintaan lisätään 14% ALV oston yhteydessä.
                         </small>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group  required">
                         <label htmlFor="exampleAmount">Määrä kiloina</label>
                         <input type="text" className="form-control" id="exampleAmount" placeholder="Määrä(kg)"/>
                     </div>
-                    <label htmlFor="exampleAmount">Saatavilla:</label>
+                    <div className="required">
+                        <label htmlFor="exampleAmount">Saatavilla:</label>
+                    </div>
                     <div className="row">
                         <div className="col">
                             <input type="date" className="form-control" />
@@ -33,7 +36,7 @@ class AddProduct extends Component {
                             <input type="date" className="form-control" />
                         </div>
                     </div>
-
+                    <br />
                     <div className="form-group">
                         <label htmlFor="exampleInfo">Lisätiedot</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
