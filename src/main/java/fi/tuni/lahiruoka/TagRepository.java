d4694bfbd9119856cb0fc9e86b229a0e5864fbfd
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-04 11:36:27 +0300

    TagREpository: add method to find a tag by name (ignoring case)

--- src/main/java/fi/tuni/lahiruoka/TagRepository.java
+++ src/main/java/fi/tuni/lahiruoka/TagRepository.java
@@ -3,7 +3,10 @@ package fi.tuni.lahiruoka;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-
+    Optional<Tag> findByNameIgnoreCase(String name);
 }
