863e103ab68c3e894723550238738d5dc37eb428
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-09 14:01:25 +0300

    UserRepository: add method for finding user by googleId

--- src/main/java/fi/tuni/lahiruoka/UserRepository.java
+++ src/main/java/fi/tuni/lahiruoka/UserRepository.java
@@ -3,6 +3,9 @@ package fi.tuni.lahiruoka;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findUserByGoogleId(String googleId);
 }
