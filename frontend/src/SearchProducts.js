6f314ad11765daa4cfda96a67ec4098c2f1b9157
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-11 20:40:04 +0300

    SearchProducts: Initial links for sorting

--- frontend/src/SearchProducts.js
+++ frontend/src/SearchProducts.js
@@ -1,17 +1,31 @@
 import React, {Component} from "react";
 import './css/ProductPage_style.css';
+import './css/SearchProducts_style.css';
 import SingleProduct from './SingleProduct';
 
 class SearchProducts extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {productList: []}
+        console.log(props.match.params.keyWord);
+        this.state = {productList: [], text: 'Lataa...', sort: 'Newest'}
         this.listAllProducts = this.listAllProducts.bind(this);
+        this.writeText = this.writeText.bind(this);
+        this.sort = this.sort.bind(this);
     }
 
     componentDidMount() {
-        fetch('/api/products/').then((httpResponse) => httpResponse.json()).then(this.listAllProducts);
+        fetch('/api/search/' + this.props.match.params.keyWord).then((httpResponse) => httpResponse.json()).then(this.listAllProducts);
+        this.writeText();
+    }
+
+    sort(event) {
+        event.preventDefault();
+        console.log(event.target.id);
+    }
+
+    writeText() {
+        this.setState({text: <p><a id="klikattu" href="" onClick={this.sort}>Viimeksi lisätyt - Kilohinta nouseva - Kilohinta laskeva - Saatavilla nouseva - Saatavilla laskeva</a></p>})
     }
 
     listAllProducts(jsonObject) {
@@ -27,7 +41,10 @@ class SearchProducts extends Component {
     render() {
         return (
             <div className="product-list-container">
-                <h3 className="listTitle">Tuotteet</h3>
+                <div className="textContainer">
+                    <h3 className="listTitle">Hakutulokset sanalle "{this.props.match.params.keyWord}"</h3>
+                    {this.state.text}
+                </div>
                 {this.state.productList}
             </div>
         )
