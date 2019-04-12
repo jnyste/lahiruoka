16bf85ba8bc5cc2882a3baf8434b7f261d6ed50e
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-12 10:26:17 +0300

    Wrapper, NavigationBar.js: detach NavBar into its own React component

--- frontend/src/Wrapper.js
+++ frontend/src/Wrapper.js
@@ -8,7 +8,8 @@ import UserProfile from './UserProfile';
 import AddProduct from './AddProduct';
 import AddUser from './AddUser'
 import Login from './Login';
-import SearchProducts from './SearchProducts'
+import SearchProducts from './SearchProducts';
+import NavigationBar from './NavigationBar';
 
 function Index() {
   return <FarmCarousel />;
@@ -29,26 +30,7 @@ function Wrapper() {
   return (
     <Router>
       <div>
-        <Navbar className="header" expand="lg">
-        <Navbar.Brand className="navLogo" style={{float: "left"}} href="/">Sivun logo</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse className="navCollapse" id="basic-navbar-nav">
-            <Form className="navForm" inline>
-              <FormControl type="text" placeholder="Hae tuotetta tai tuottajaa.." className="search" />
-              <Button className="navFormButton" variant="light">Hae</Button>
-            </Form>
-            <Nav className="navLinks">
-              <Nav.Link className="navProducts" href="/tuotteet/">TUOTTEET</Nav.Link>
-              <NavDropdown className="navAccount" title="OMA TILI" id="userDropdown">
-                <NavDropdown.Item href="/login/">OMA TILI</NavDropdown.Item>
-                <NavDropdown.Item>{<Login/>}</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown className="navShoppingcart" title="OSTOSKORI (0)" id="shoppingcartDropdown">
-                <NavDropdown.Item href="/ostoskori/" >OSTOSKORI</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <NavigationBar />
 
         <Route path="/" exact component={Index} />
         <Route path="/tuotteet/" component={Products} />
