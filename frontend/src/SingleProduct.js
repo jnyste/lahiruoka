b862b647529547885dabf60187d38a3a32faf035
Essi Supponen <essi.supponen@cs.tamk.fi>
2019-04-12 13:23:28 +0300

    SingleProduct.js: Add availableTo date

--- frontend/src/SingleProduct.js
+++ frontend/src/SingleProduct.js
@@ -6,6 +6,10 @@ class SingleProduct extends Component {
     constructor(properites) {
         super(properites);
 
+        this.updateListener = this.updateListener.bind(this);
+        this.addListener = this.addListener.bind(this);
+        this.writeDate = this.writeDate.bind(this);
+
         let productTags = '';
 
         for (let tag of this.props.id.tags) {
@@ -16,10 +20,23 @@ class SingleProduct extends Component {
                     , tags: productTags
                     , total: 0
                     , totalTax: 0};
+    }
 
-        this.updateListener = this.updateListener.bind(this);
-        this.addListener = this.addListener.bind(this);
+    writeDate(dateString) {
+        let date = '';
+        let dateSplitted = dateString.split('-');
 
+        if (dateSplitted[1].substring(0,1) === '0') {
+            dateSplitted[1] = dateSplitted[1].substring(1,2);
+        }
+
+        if (dateSplitted[2].substring(0,1) === '0') {
+            dateSplitted[2] = dateSplitted[2].substring(1,2);
+        }
+
+        date = dateSplitted[2] + '.' + dateSplitted[1] + '.' + dateSplitted[0];
+
+        return date;
     }
 
     updateListener(event) {
@@ -46,7 +63,8 @@ class SingleProduct extends Component {
                         <h1 className="price">{this.props.id.price} €/kg</h1>
                         <h3 className="product title">{this.props.id.name}</h3>
                         <p className="product owner">{this.props.id.farm.companyName}</p>
-                        <p className="productAmount">Saatavilla yht. {this.props.id.amount} kg</p>
+                        <p className="productAmount">Yhteensä {this.props.id.amount} kg</p>
+                        <p className="productAmount">Saatavilla {this.writeDate(this.props.id.availableTo)} saakka</p>
                         <p className="productExtraInfo">Lisätiedot: {this.props.id.info}</p>
                         <p className="tags">{this.state.tags}</p>
                     </div>
