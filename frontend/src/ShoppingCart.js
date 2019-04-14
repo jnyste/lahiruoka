7ac1f06457af3717f4bb1bd6851205e3ef850f43
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-14 21:37:15 +0300

    ShoppingCart.js: Add order amount fetch to farmer.

--- frontend/src/ShoppingCart.js
+++ frontend/src/ShoppingCart.js
@@ -6,9 +6,11 @@ class ShoppingCart extends Component {
         super(props);
         let status = (localStorage.getItem('loggedin') === 'true') ? 'loggedin' : 'loggedout';
         console.log('status: ' + status);
-        this.state = {status: status};
+        this.state = {status: status
+                    , orderAmount: 0};
         this.loggedoutRender = this.loggedoutRender.bind(this);
         this.loggedinRender = this.loggedinRender.bind(this);
+        this.checkFarmOrders = this.checkFarmOrders.bind(this);
     }
 
     componentDidMount() {
@@ -23,10 +25,21 @@ class ShoppingCart extends Component {
         if(localStorage.getItem('userType') === 'KITCHEN') {
             return (<p>Keittiö.</p>);
         } else if (localStorage.getItem('userType') === 'FARM') {
-            return (<p>Sinulle ei ole juuri nyt tilauksia.</p>);
+            return (<p>Sinulle on {this.checkFarmOrders()} tilausta.</p>);
         } else {
             return (<p>ERROR, kokeile kirjautua ulos ja takaisin sisään.</p>);
         }
+    }
+
+    checkFarmOrders() {
+        let orderAmount = 0;
+        fetch('/api/users/' + localStorage.getItem('userId') + '/orders')
+            .then((resp) => resp.json())
+            .then((orders) => {
+                console.log('user orders length: ', orders.length);
+                orderAmount= orders.length;
+            });
+        return orderAmount;
     }
 
     render() {
