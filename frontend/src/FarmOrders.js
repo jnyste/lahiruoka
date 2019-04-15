9173b3909fb70c3c64734cdf83e37981b77bd77c
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 13:29:39 +0300

    FarmOrders.js: Separate orders into two arrays: new and accepted.

--- frontend/src/FarmOrders.js
+++ frontend/src/FarmOrders.js
@@ -7,7 +7,8 @@ class FarmOrders extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            orders: []
+            newOrders: []
+            , acceptedOrders: []
         }
     }
 
@@ -15,11 +16,18 @@ class FarmOrders extends Component {
         fetch('/api/users/' + localStorage.getItem('userId') + '/orders')
             .then((resp) => resp.json())
             .then((orders) => {
-                let helperArray = [];
+                let newArray = [];
+                let acceptedArray = [];
                 for (let order of orders) {
-                    helperArray.push(<SingleOrder key={order.orderId} order={order}/>);
+                    if(!order.acceptedByFarmer) {
+                        newArray.push(<SingleOrder key={order.orderId} order={order}/>);
+                    } else {
+                        acceptedArray.push(<SingleOrder key={order.orderId} order={order}/>);
+                    }
+
                 }
-                this.setState({orders: helperArray});
+                this.setState({newOrders: newArray
+                    , acceptedOrders: acceptedArray});
             });
     }
 
@@ -27,8 +35,9 @@ class FarmOrders extends Component {
         return(
             <div className="ordercontainer">
                 <h1 className="ordertitle">Hyväksymättömät tilaukset</h1>
-                {this.state.orders}
+                {this.state.newOrders}
                 <h1 className="ordertitle">Hyväksytyt tilaukset</h1>
+                {this.state.acceptedOrders}
             </div>
         )
     }
