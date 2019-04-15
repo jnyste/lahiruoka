882fb46c70a20eb84692e3e33882663d2304443f
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 10:53:55 +0300

    Create OrdersPage.js

---
+++ frontend/src/OrdersPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class OrdersPage extends Component {
+
+    render() {
+        return(
+            <h1>Tilausket</h1>
+        )
+    }
+}
+
+export default OrdersPage;
