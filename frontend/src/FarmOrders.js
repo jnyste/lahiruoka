ada24722a89462337987ea9aedc75b7d514856d5
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 12:35:21 +0300

    FarmOrders.js, KitchenOrders.js: Add pages.

---
+++ frontend/src/FarmOrders.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class FarmOrders extends Component {
+
+
+
+    render() {
+        return(
+            <h1>FarmOrders</h1>
+        )
+    }
+}
+
+export default FarmOrders;
---
+++ frontend/src/KitchenOrders.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class KitchenOrders extends Component {
+
+
+    render() {
+        return(
+            <h1>KitchenOrders</h1>
+        )
+    }
+}
+
+export default KitchenOrders;
