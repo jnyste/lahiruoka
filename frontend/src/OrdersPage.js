f8e59b711c938c71f471ca7cc33657787fd6458a
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 12:41:38 +0300

    OrdersPage.js: Add check for what is rendered.

--- frontend/src/OrdersPage.js
+++ frontend/src/OrdersPage.js
@@ -1,11 +1,30 @@
 import React, {Component} from "react";
+import ErrorPage from './ErrorPage';
+import FarmOrders from "./FarmOrders";
+import KitchenOrders from "./KitchenOrders";
 
 class OrdersPage extends Component {
 
     render() {
-        return(
-            <h1>Tilausket</h1>
-        )
+        console.log(localStorage);
+        if(localStorage.getItem('loggedin') === 'true') {
+            console.log('loggedin')
+            if(localStorage.getItem('userType') === 'FARM') {
+                console.log('farmikäyttäjä')
+                return <FarmOrders/>
+            } else if(localStorage.getItem('userType') === 'KITCHEN') {
+                console.log('keittiökäyttäjä')
+                return <KitchenOrders/>
+            } else {
+                console.log('login mut error')
+                return <ErrorPage/>
+            }
+        } else {
+            console.log('ei ollu kirjauduttu sisää')
+            return(
+                <ErrorPage/>
+            )
+        }
     }
 }
 
