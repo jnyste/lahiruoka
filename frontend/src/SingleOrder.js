6e180b47b8a9ccd424e07a2b184a6221dbde164d
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 13:51:34 +0300

    SingleOrder.js: Add div for text portion.

--- frontend/src/SingleOrder.js
+++ frontend/src/SingleOrder.js
@@ -9,23 +9,24 @@ class SingleOrder extends Component {
     render() {
         return (
             <div className="singleordercontainer">
-                <h3>Tilattu tuote: {this.props.order.product.name}</h3>
-                <p>Tilauspäivä: {this.props.order.dateOfOrder}</p>
-                <p>Määrä: {this.props.order.amount} kg</p>
-                <p>Hinta: {this.props.order.product.price} €/kg</p>
-                <br/>
-                <p>Tilaaja: {this.props.order.orderer.companyName}</p>
-                <p>Toimitusosoite: {this.props.order.orderer.address}</p>
-                <p>Puhelinnumero: {this.props.order.orderer.phone}</p>
-                <br/>
-                <p>Toimituspäivä: {this.props.order.dateOfDelivery}</p>
-                {!this.props.order.acceptedByFarmer &&
-                    <div className="orderbutton">
-                        <button className="acceptOrderButton">Hyväksy</button>
-                        <button className="declineOrderButton">Kieltäydy tilauksesta</button>
-                    </div>
-                }
-
+                <div className="ordertext">
+                    <h3>Tilattu tuote: {this.props.order.product.name}</h3>
+                    <p>Tilauspäivä: {this.props.order.dateOfOrder}</p>
+                    <p>Määrä: {this.props.order.amount} kg</p>
+                    <p>Hinta: {this.props.order.product.price} €/kg</p>
+                    <br/>
+                    <p>Tilaaja: {this.props.order.orderer.companyName}</p>
+                    <p>Toimitusosoite: {this.props.order.orderer.address}</p>
+                    <p>Puhelinnumero: {this.props.order.orderer.phone}</p>
+                    <br/>
+                    <p>Toimituspäivä: {this.props.order.dateOfDelivery}</p>
+                    {!this.props.order.acceptedByFarmer &&
+                        <div className="orderbutton">
+                            <button className="acceptOrderButton">Hyväksy</button>
+                            <button className="declineOrderButton">Kieltäydy tilauksesta</button>
+                        </div>
+                    }
+                </div>
             </div>
         );
     }
