fda9d9c60a9adf72a798bf416bbc5a890e57c605
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 12:58:19 +0300

    FarmOrders.js: Add order fetch.

--- frontend/src/FarmOrders.js
+++ frontend/src/FarmOrders.js
@@ -2,11 +2,36 @@ import React, {Component} from "react";
 
 class FarmOrders extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            orders: []
+        }
+    }
 
+    componentDidMount() {
+        fetch('/api/users/' + localStorage.getItem('userId') + '/orders')
+            .then((resp) => resp.json())
+            .then((orders) => {
+                let helperArray = [];
+                for (let order of orders) {
+                    helperArray.push(order);
+                }
+                this.setState({orders: helperArray});
+            });
+    }
 
     render() {
+        let allOrders = this.state.orders;
         return(
-            <h1>FarmOrders</h1>
+            <div>
+                <h1>Tilaukset sinulle</h1>
+                <ul>
+                {allOrders.map((order) =>
+                    <li key={order.orderId}>{'Tilausnumero: ' + order.orderId +', tuote: ' + order.product.name + ', tilattu määrä: ' + order.amount
+                    + 'kg, tilaaja: ' + order.orderer.companyName + ' osoite: ' + order.orderer.address + ', puhelinnro: ' + order.orderer.phone}</li>)}
+                </ul>
+            </div>
         )
     }
 }
