0e9117f4ca061f1998cc40808327d999108920ee
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-15 12:59:41 +0300

    Create SingleOrder.js

---
+++ frontend/src/SingleOrder.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class SingleOrder extends Component {
+    render() {
+        return (
+            <h1>Tää on yksittäinen order</h1>
+        )
+    }
+}
+
+export default SingleOrder;
