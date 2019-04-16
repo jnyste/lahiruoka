67d15d5af55e6975127ffe93aa0252ce67c00fe2
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-04-16 20:35:28 +0300

    SingleOrder.js: Add orders as Collapsible.

--- frontend/src/SingleOrder.js
+++ frontend/src/SingleOrder.js
@@ -1,42 +1,45 @@
 import React, {Component} from "react";
 import './css/OrdersPage_style.css';
+import Collapsible from "react-collapsible";
 
 class SingleOrder extends Component {
 
     render() {
         return (
             <div className="singleordercontainer">
-                <div className="ordertext">
-                    <h4>Tilattu tuote: {this.props.order.product.name}</h4>
-                    <p>Tilauspäivä: {this.props.order.dateOfOrder}</p>
-                    <p>Määrä: {this.props.order.amount} kg</p>
-                    <p>Hinta: {this.props.order.product.price} €/kg</p>
-                    <br/>
-                    <p>Tilaaja: {this.props.order.orderer.companyName}</p>
-                    <p>Toimitusosoite: {this.props.order.orderer.address}</p>
-                    <p>Puhelinnumero: {this.props.order.orderer.phone}</p>
-                    <br/>
-                    <p>Toivottu toimituspäivä ja -aika: {this.props.order.dateOfDelivery} klo {this.props.order.timeOfDelivery.substring(0,5)}</p>
-                    {localStorage.getItem('userType') === 'KITCHEN' ?
-                        (!this.props.order.confirmedByOrderer &&
-                            <div className="orderbutton">
-                                <button className="acceptOrderButton" onClick={this.props.updateOrders} name="confirm"
-                                        value={this.props.order.orderId}>Lähetä tilaus
-                                </button>
-                                <button className="declineOrderButton" onClick={this.props.updateOrders} name="cancel"
-                                        value={this.props.order.orderId}>Peruuta
-                                </button>
-                            </div>
-                        )
-                        :
-                        ((!this.props.order.acceptedByFarmer && !this.props.order.declinedByFarmer) &&
-                            <div className="orderbutton">
-                            <button className="acceptOrderButton" onClick={this.props.updateOrders} name="accept" value={this.props.order.orderId}>Hyväksy</button>
-                            <button className="declineOrderButton" onClick={this.props.updateOrders} name="decline" value={this.props.order.orderId}>Kieltäydy tilauksesta</button>
-                            </div>
-                        )
-                    }
-                </div>
+                <Collapsible trigger={"terppa"} triggerClassName="producttitletrigger" triggerOpenedClassName="productopenedtrigger" >
+                    <div className="ordertext">
+                        <h4>Tilattu tuote: {this.props.order.product.name}</h4>
+                        <p>Tilauspäivä: {this.props.order.dateOfOrder}</p>
+                        <p>Määrä: {this.props.order.amount} kg</p>
+                        <p>Hinta: {this.props.order.product.price} €/kg</p>
+                        <br/>
+                        <p>Tilaaja: {this.props.order.orderer.companyName}</p>
+                        <p>Toimitusosoite: {this.props.order.orderer.address}</p>
+                        <p>Puhelinnumero: {this.props.order.orderer.phone}</p>
+                        <br/>
+                        <p>Toivottu toimituspäivä ja -aika: {this.props.order.dateOfDelivery} klo {this.props.order.timeOfDelivery.substring(0,5)}</p>
+                        {localStorage.getItem('userType') === 'KITCHEN' ?
+                            (!this.props.order.confirmedByOrderer &&
+                                <div className="orderbutton">
+                                    <button className="acceptOrderButton" onClick={this.props.updateOrders} name="confirm"
+                                            value={this.props.order.orderId}>Lähetä tilaus
+                                    </button>
+                                    <button className="declineOrderButton" onClick={this.props.updateOrders} name="cancel"
+                                            value={this.props.order.orderId}>Peruuta
+                                    </button>
+                                </div>
+                            )
+                            :
+                            ((!this.props.order.acceptedByFarmer && !this.props.order.declinedByFarmer) &&
+                                <div className="orderbutton">
+                                <button className="acceptOrderButton" onClick={this.props.updateOrders} name="accept" value={this.props.order.orderId}>Hyväksy</button>
+                                <button className="declineOrderButton" onClick={this.props.updateOrders} name="decline" value={this.props.order.orderId}>Kieltäydy tilauksesta</button>
+                                </div>
+                            )
+                        }
+                    </div>
+                </Collapsible>
             </div>
         );
     }
